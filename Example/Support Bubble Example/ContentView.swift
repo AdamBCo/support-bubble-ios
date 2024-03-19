1b9148ea419388ee1fa2d760ae0c8b359269de88
Adam Cooper <adambco@me.com>
2024-03-19 08:18:35 -0700

    Update

--- Example/Support Bubble Example/ContentView.swift
+++ Example/Support Bubble Example/ContentView.swift
@@ -10,20 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-            
-            NavigationLink {
-                Tickets
-            } label: {
-                <#code#>
-            }
-
-        }
-        .padding()
+        TicketsListView()
     }
 }
 
