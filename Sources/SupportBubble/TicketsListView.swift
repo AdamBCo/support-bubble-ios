d144cb31f7dfcd0057216007d544e1a1c5a1f48e
Adam Cooper <adambco@me.com>
2024-03-19 08:22:20 -0700

    Add init

--- Sources/SupportBubble/TicketsListView.swift
+++ Sources/SupportBubble/TicketsListView.swift
@@ -9,6 +9,11 @@ import SwiftUI
 
 @available(iOS 13.0.0, *)
 public struct TicketsListView: View {
+    
+    public init() {
+        
+    }
+    
     public var body: some View {
         Text("TicketsListView")
     }
