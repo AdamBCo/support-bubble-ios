d791858a4b9d209a345503516dbec30d3af68875
Adam Cooper <adambco@me.com>
2024-03-19 08:00:51 -0700

    Add example

---
+++ Example/Support Bubble Example/Support_Bubble_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Support_Bubble_ExampleApp.swift
+//  Support Bubble Example
+//
+//  Created by Adam Cooper on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Support_Bubble_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
