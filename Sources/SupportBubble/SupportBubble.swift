0950aa55a5788848d944f1a0805b1f640f8db52d
Adam Cooper <adambco@me.com>
2024-03-20 16:13:01 -0700

    Update

--- Sources/SupportBubble/SupportBubble.swift
+++ Sources/SupportBubble/SupportBubble.swift
@@ -1,5 +1,8 @@
 import Foundation
+import SwiftUI
+import UIKit
 
+@available(iOS 16.0, *)
 public class SupportBubble {
     
     public static let shared = SupportBubble()
@@ -42,6 +45,33 @@ public class SupportBubble {
     
     public func logout() {
         UserDefaults.standard.removeObject(forKey: UserDefaultKeys.token.rawValue)
+    }
+    
+    // MARK: - Private Methods
+    private func topViewController(_ base: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
+        if let nav = base as? UINavigationController {
+            return topViewController(nav.visibleViewController)
+        } else if let tab = base as? UITabBarController, let selected = tab.selectedViewController {
+            return topViewController(selected)
+        } else if let presented = base?.presentedViewController {
+            return topViewController(presented)
+        }
+        return base
+    }
+        
+    func presentTicketsScreen() {
+        // Assuming TicketsScreen conforms to View
+        let ticketsView = TicketsListView()
+        let hostingController = UIHostingController(rootView: ticketsView)
+        
+        // Ensure we're on the main thread since UI changes must be on the main thread
+        DispatchQueue.main.async {
+            if let topVC = self.topViewController() {
+                topVC.present(hostingController, animated: true, completion: nil)
+            } else {
+                print("Error: Could not find top view controller.")
+            }
+        }
     }
     
 }
